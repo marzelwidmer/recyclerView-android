5daebd1283d5e33f0780e6d7a542f720712ad4ff
Marcel Widmer <marzelwidmer@gmail.com>
2018-04-14 15:10:15 +0200

    work on video row content

--- app/src/main/java/ch/keepcalm/recyclerview_android/MainAdapter.kt
+++ app/src/main/java/ch/keepcalm/recyclerview_android/MainAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.video_row.view.*
 
 class MainAdapter : RecyclerView.Adapter<CustomerViewHolder>() {
 
@@ -21,10 +22,12 @@ class MainAdapter : RecyclerView.Adapter<CustomerViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: CustomerViewHolder, position: Int) {
+        holder.view.textView_video_title.text = "123"
+
     }
 
 
 
 }
 
-class CustomerViewHolder(v: View): RecyclerView.ViewHolder(v)
+class CustomerViewHolder(val view: View): RecyclerView.ViewHolder(view)
