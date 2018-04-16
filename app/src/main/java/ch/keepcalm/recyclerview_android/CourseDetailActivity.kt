17319623c9dec32a30fa76e18c5262b076893875
Marcel Widmer <marzelwidmer@gmail.com>
2018-04-16 12:02:17 +0200

    add detail view course lesson row

--- app/src/main/java/ch/keepcalm/recyclerview_android/CourseDetailActivity.kt
+++ app/src/main/java/ch/keepcalm/recyclerview_android/CourseDetailActivity.kt
@@ -2,6 +2,8 @@ package ch.keepcalm.recyclerview_android
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * Created by  marcelwidmer on 16.04.18 - 16/04/April
@@ -11,6 +13,10 @@ class CourseDetailActivity:AppCompatActivity(){
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
+
+            setContentView(R.layout.activity_main)
+            recycleView_main.layoutManager = LinearLayoutManager(this)
+            recycleView_main.adapter = CourseDetailAdapter()
         }
 
 }
---
+++ app/src/main/java/ch/keepcalm/recyclerview_android/CourseDetailAdapter.kt
@@ -0,0 +1,45 @@
+package ch.keepcalm.recyclerview_android
+
+import android.content.Intent
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CourseDetailAdapter : RecyclerView.Adapter<CourseLessonViewHolder>() {
+
+    // number of items
+    override fun getItemCount(): Int {
+        return 10
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseLessonViewHolder {
+
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val customView = layoutInflater.inflate(R.layout.course_lesson_row, parent, false)
+
+
+//
+//        val readView = View(parent?.context)
+//        readView.setBackgroundColor(Color.BLACK)
+//        readView.minimumHeight = 50
+
+        return CourseLessonViewHolder(customView)
+
+    }
+
+    override fun onBindViewHolder(holder: CourseLessonViewHolder, position: Int) {
+        //
+    }
+
+
+}
+
+class CourseLessonViewHolder(val view: View) : RecyclerView.ViewHolder(view){
+    init{
+        view.setOnClickListener{
+            val intent = Intent(view.context, CourseDetailActivity::class.java)
+            view.context.startActivity(intent)
+        }
+    }
+}
